896388f98d30d797973cd1bc4ee78eac4c84fda8
theaikea <thea0604@gmail.com>
2024-05-02 16:09:55 +0200

    css

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <template>
-  <div class="container mx-auto">
-    <div class="text-center">
-      <h1 class="text-5xl mt-36">KÆRLIGHEDSREGNEMASKINEN</h1>
-      <form @submit.prevent="calculateLove" class="mt-8">
-        <div class="input-container">
-          <label for="yourName" class="font-bold text-xl block">DIT NAVN:</label>
-          <input type="text" id="yourName" v-model="yourName" required class="input-field mb-4">
-        </div>
-        <div class="input-container">
-          <label for="partnerName" class="font-bold text-xl block">DIT CRUSH NAVN:</label>
-          <input type="text" id="partnerName" v-model="partnerName" required class="input-field mb-4">
-        </div>
-        <button type="submit" class="px-10 py-4 text-xl bg-pink-500 text-white rounded-lg cursor-pointer transition-colors hover:bg-pink-600">UDREGN</button>
-      </form>
-      <div class="mt-8">
-        <div v-if="showLovePercentage" class="font-bold text-2xl">{{ yourName }} og {{ partnerName }} har en kærlighedsprocent på:</div>
-        <div class="text-2xl">{{ lovePercentage }}%</div>
-        <div class="w-full h-16 bg-gray-300 rounded-full mt-4">
-        
-          <div :style="{ width: progressBarWidth }" class="progress-bar h-full text-xl text-white text-center bg-pink-500 rounded-full"></div>
+  <div>
+    <div class="container mx-auto py-8">
+      <div class="text-center">
+        <h1 class="text-7xl mt-28">KÆRLIGHEDSREGNEMASKINEN</h1>
+        <form @submit.prevent="calculateLove" class="mt-8">
+          <div class="input-container">
+            <label for="yourName" class="font-bold text-xl block">DIT NAVN:</label>
+            <input type="text" id="yourName" v-model="yourName" required class="input-field mb-4">
+          </div>
+          <div class="input-container">
+            <label for="partnerName" class="font-bold text-xl block">DIT CRUSH NAVN:</label>
+            <input type="text" id="partnerName" v-model="partnerName" required class="input-field mb-4">
+          </div>
+          <button type="submit" class="px-10 py-4 text-xl bg-pink-500 text-white rounded-lg cursor-pointer transition-colors hover:bg-pink-600">UDREGN</button>
+        </form>
+        <div class="mt-8">
+          <div v-if="showLovePercentage" class="font-bold text-2xl">{{ yourName }} og {{ partnerName }} har en kærlighedsprocent på:</div>
+          <div class="text-2xl">{{ lovePercentage }}%</div>
+          <div class="w-full h-16 bg-gray-300 rounded-full mt-4">
+            <div :style="{ width: progressBarWidth }" class="progress-bar h-full text-xl text-white text-center bg-pink-500 rounded-full"></div>
+          </div>
         </div>
       </div>
     </div>
@@ -88,5 +89,7 @@ export default {
   box-sizing: border-box;
 }
 
-
+body {
+  background-color: #fabcc7; /* Light pink background color */
+}
 </style>
